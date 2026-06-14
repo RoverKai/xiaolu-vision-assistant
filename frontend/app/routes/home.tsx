99de4abe44a33b8ef32e4656ec3111c87714aa61
roverkai <2196140098@qq.com>
2026-06-14 22:32:16 +0800

    style(chat): remove text label from send button, show icon only

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1633,7 +1633,6 @@ export default function Home() {
                       disabled={!manualTranscript.trim() && !wakeTranscript.trim()}
                     >
                       <Icon name="icon-send" />
-                      <span>发送</span>
                     </button>
                   </div>
 
